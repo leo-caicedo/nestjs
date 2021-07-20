7aa98c643010f370ba0dba791e8c529070804ceb
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-07-20 15:49:51 -0500

    Configuración por ambientes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
+    //@Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
   ) {}
   getHello(): string {
-    console.log(this.tasks);
-    return `Hello World! API_KEY ${this.apiKey}`;
+    const apiKey = this.config.get<string>('API_KEY');
+    const db = this.config.get('DATABASE_NAME');
+    return `Hello World! API_KEY ${apiKey} ${db}`;
   }
 }
