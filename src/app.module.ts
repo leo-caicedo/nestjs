14f2e23ab6434508bf392b331eca2c0fdf298dfe
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-07-20 19:31:14 -0500

    Validación de esquemas en .envs con Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
+// Third party app first position
 import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+// Your dependencies
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,12 +11,19 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
 import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    //@Inject('API_KEY') private apiKey: string,
+    // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
-    private config: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
   getHello(): string {
-    const apiKey = this.config.get<string>('API_KEY');
-    const db = this.config.get('DATABASE_NAME');
-    return `Hello World! API_KEY ${apiKey} ${db}`;
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+    return `Hello World! ${apiKey} ${name}`;
   }
 }
